e8c55cc86d9f0c65185b0d5b0ba4bdeaef7f3b4a
arcasoyece <arcasoyece@gmail.com>
2026-03-27 23:14:59 +0300

    Add sudoku_validator function

--- sudoku.py
+++ sudoku.py
@@ -5,23 +5,24 @@ def sudoku_validator(grid):
         return set(group) == set(range(1, 10))
 
     # Satır kontrolü
-    for row in grid:
+    for i in range(9):  # i = satır indexi
+        row = grid[i]
         if not is_valid_group(row):
             return False
 
     # Sütun kontrolü
-    for col in range(9):
-        column = [grid[row][col] for row in range(9)]
+    for j in range(9):  # j = sütun indexi
+        column = [grid[i][j] for i in range(9)]
         if not is_valid_group(column):
             return False
 
     # 3x3 kare kontrolü
-    for box_row in range(0, 9, 3):
-        for box_col in range(0, 9, 3):
+    for box_i in range(0, 9, 3):  # 3x3 blok satır başlangıcı
+        for box_j in range(0, 9, 3):  # 3x3 blok sütun başlangıcı
             box = []
-            for i in range(3):
-                for j in range(3):
-                    box.append(grid[box_row + i][box_col + j])
+            for i in range(3):  # blok içindeki satır
+                for j in range(3):  # blok içindeki sütun
+                    box.append(grid[box_i + i][box_j + j])
             if not is_valid_group(box):
                 return False
 
